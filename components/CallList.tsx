553581f58ba1467e4ea596d581f3508c12f6b90e
Manoj Kumar <manojkumarba32@gmail.com>
2025-04-15 19:16:21 +0530

    'changed layout'

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -64,7 +64,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   const noCallsMessage = getNoCallsMessage();
 
   return (
-    <div className="grid grid-cols-1 gap-5 xl:grid-cols-3">
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
       {calls && calls.length > 0 ? (
         calls.map((meeting: Call | CallRecording) => (
           <MeetingCard
